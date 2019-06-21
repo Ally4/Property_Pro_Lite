2a9a332b73465ff463714937108354875c7346ae
Ally4 <el.ally741@gmail.com>
2019-06-21 18:00:30 +0200

    [chore #166815672] there an add of datas for the signup.code

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
+app.get('/signup', (req, res, next) => {
+    const signup = [
+        {
+        id: 1,
+        email: 'signup@signup.com',
+        First_name: "Sign",
+        last_name: "up",
+        password: "password",
+        phone_number: "+250 784403223",
+        address: "Kigali",
+        is_admin: true,
        message: "Everyday is a good day to learn" 
-    });
+    },
+];
+res.status(200).json(signup);
 });
 
 console.log("The server is listened to 4000 ...");
